d5a9af4099e6047c34d60d4a56328f1ba795aa36
Andrew Jackson <andrew.d.jackson@gmail.com>
2021-01-22 16:50:55 +0000

    unified single response map

--- responder.go
+++ responder.go
@@ -42,19 +42,15 @@ type ScenarioDescription struct {
 
 // Responder struct
 type Responder struct {
-	file        *os.File
-	RawData     []*RawData
-	Playbook    Playbook
-	responseMap map[string][]byte
+	file     *os.File
+	RawData  []*RawData
+	Playbook Playbook
+	//responseMap map[string][]byte
 }
 
 // NewResponder creates an instance of a responder
 func NewResponder() *Responder {
 	responder := &Responder{}
-	responder.responseMap = make(map[string][]byte)
-
-	responder.buildResponseMap()
-
 	return responder
 }
 
@@ -178,52 +174,13 @@ func (responder *Responder) convertHexStringToByteArray(response string) []byte 
 func (responder *Responder) generateECUResponse(command string) []byte {
 	command = strings.ToUpper(command)
 
-	r := responder.responseMap[command]
+	r := responseMap[command]
 
 	if r == nil {
-		r = responder.responseMap["00"]
+		r = responseMap["00"]
 		copy(r[0:], command)
 	}
 
 	log.WithFields(log.Fields{"response": fmt.Sprintf("%x", r), "command": command}).Info("generated response for command")
 	return r
-}
-
-// build the response map for generated Responses
-func (responder *Responder) buildResponseMap() {
-	// Response formats for commands that do not respond with the format [COMMAND][VALUE]
-	// Generally these are either part of the initialisation sequence or are ECU data frames
-	responder.responseMap["0A"] = []byte{0x0A}
-	responder.responseMap["CA"] = []byte{0xCA}
-	responder.responseMap["75"] = []byte{0x75}
-
-	// Format for DataFrames starts with [Command Echo][Data Size][Data Bytes (28 for 0x80 and 32 for 0x7D)]
-	responder.responseMap["80"] = []byte{0x80, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B}
-	responder.responseMap["7D"] = []byte{0x7d, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F}
-	responder.responseMap["D0"] = []byte{0xD0, 0x99, 0x00, 0x03, 0x03}
-
-	// heatbeat
-	responder.responseMap["F4"] = []byte{0xf4, 0x00}
-	responder.responseMap["FB"] = []byte{0xfb, 0x00}
-
-	// adjustments
-	responder.responseMap["7A"] = []byte{0x7a, 0x89}
-	responder.responseMap["7B"] = []byte{0x7b, 0x1e}
-	responder.responseMap["7C"] = []byte{0x7c, 0x8a}
-	responder.responseMap["79"] = []byte{0x79, 0x8a}
-	responder.responseMap["7B"] = []byte{0x7b, 0x8a}
-	responder.responseMap["8A"] = []byte{0x8a, 0x23}
-	responder.responseMap["89"] = []byte{0x89, 0x23}
-	responder.responseMap["92"] = []byte{0x92, 0x80}
-	responder.responseMap["91"] = []byte{0x91, 0x80}
-	responder.responseMap["94"] = []byte{0x94, 0x80}
-	responder.responseMap["93"] = []byte{0x93, 0x80}
-
-	//resets
-	responder.responseMap["FA"] = []byte{0xfa, 0x00}
-	responder.responseMap["0F"] = []byte{0x0f, 0x00}
-	responder.responseMap["CC"] = []byte{0xcc, 0x00}
-
-	// generic response, expect command and single byte response
-	responder.responseMap["00"] = []byte{0x00, 0x00}
 }
