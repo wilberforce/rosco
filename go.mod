636d30208ef4649048ca7d0532770766b7e3eaa8
Andrew Jackson <andrew.d.jackson@gmail.com>
2021-02-14 14:45:35 +0000

    initial implementation of oscillation detection

--- go.mod
+++ go.mod
@@ -6,8 +6,11 @@ require (
 	github.com/corbym/gocrest v1.0.5
 	github.com/gocarina/gocsv v0.0.0-20201208093247-67c824bc04d4
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/montanaflynn/stats v0.6.4
 	github.com/sirupsen/logrus v1.7.0
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	golang.org/x/exp v0.0.0-20210212053707-62dc52270d37 // indirect
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	gonum.org/v1/gonum v0.8.2
+	gonum.org/v1/netlib v0.0.0-20201012070519-2390d26c3658 // indirect
 )
--- stats.go
+++ stats.go
@@ -2,25 +2,25 @@ package rosco
 
 import (
 	"fmt"
+	"github.com/montanaflynn/stats"
 	log "github.com/sirupsen/logrus"
-	"math"
-
 	"gonum.org/v1/gonum/stat"
 )
 
 // Stats structure
 type Stats struct {
-	Name       string
-	Value      float64
-	Count      int
-	Max        float64
-	Min        float64
-	Mean       float64
-	Stddev     float64
-	Mode       float64
-	ModeCount  float64
-	TrendSlope float64
-	Trend      float64
+	Name        string
+	Value       float64
+	Count       int
+	Max         float64
+	Min         float64
+	Mean        float64
+	Stddev      float64
+	Mode        float64
+	ModeCount   float64
+	TrendSlope  float64
+	Trend       float64
+	Oscillation float64
 }
 
 // NewStats generates stats from a sample of float64 values
@@ -39,9 +39,13 @@ func NewStats(name string, data []float64) *Stats {
 	s.Mode, s.ModeCount = stat.Mode(data, nil)
 
 	// round to 2 decimal places
-	s.Mean = math.Round(s.Mean * 100 / 100)
-	s.Stddev = math.Round(s.Stddev * 100 / 100)
-	s.Mode = math.Round(s.Mode * 100 / 100)
+	s.Mean, _ = stats.Mean(data)
+	s.Stddev, _ = stats.StandardDeviation(data)
+	s.Oscillation, _ = stats.AutoCorrelation(data, 10)
+
+	s.Mean, _ = stats.Round(s.Mean, 2)
+	s.Stddev, _ = stats.Round(s.Stddev, 2)
+	s.Mode, _ = stats.Round(s.Mode, 2)
 	s.TrendSlope, s.Trend = linearRegression(data)
 
 	log.WithFields(log.Fields{"stats": fmt.Sprintf("%+v", *s)}).Info("diagnostic stats calculated")
